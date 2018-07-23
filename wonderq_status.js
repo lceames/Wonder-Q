9526e4e49b7f050f66ed173020e4b664e3ccaa6b
lceames <lceames@gmail.com>
2018-07-22 20:35:25 -0400

    loop to show changes in queue

--- wonderq_status.js
+++ wonderq_status.js
@@ -1,23 +1,29 @@
 const { getWonderQStatus } = require('./wonderq_client');
 
 function printResults(data, err) {
+    console.log('\033[2J');
     if (err) {
         console.log(err);
     }
     else {
         data = JSON.parse(data);
-        queueNames = Object.keys(data['queues']);
-        console.log(`${queueNames.length} queue has been created`);
-        queueNames.forEach(queueName => {
+        try {
+            queueNames = Object.keys(data['queues']);
+            console.log(`${queueNames.length} queue has been created`);
+            queueNames.forEach(queueName => {
             const queue = data['queues'][queueName];
-            console.log("--------PRINTING INDIVIDUAL QUEUE STATE--------")
-            console.log(`Queue name: ${queue['queueName']}`);
-            console.log(`Message count: ${queue['messages'].length}`)
-            console.log(`maxConsumerProcessTime: ${queue['maxConsumerProcessTime']}`)
-            console.log("---FINISHED PRINTING INDIVIDUAL QUEUE STATE---")
-        });
+                console.log("--------PRINTING INDIVIDUAL QUEUE STATE--------")
+                console.log(`Queue name: ${queue['queueName']}`);
+                console.log(`Message count: ${queue['messages'].length}`)
+                console.log(`maxConsumerProcessTime: ${queue['maxConsumerProcessTime']}`)
+                console.log("---FINISHED PRINTING INDIVIDUAL QUEUE STATE---")
+            });
+        }
+        catch {}
     }
 }
 
-getWonderQStatus({}, printResults)
+setInterval(() => getWonderQStatus({}, printResults), 1000)
+
+
 
